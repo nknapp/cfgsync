ddb582ef440f94b5100a397b1d879bbdd47bc933
Nils Knappmeier <npm@knappi.org>
2026-05-29 20:42:10 +0200

    test: fix ci pipeline

--- e2e-tests/run.sh
+++ e2e-tests/run.sh
@@ -2,14 +2,16 @@
 
 set -eu
 
-cd "$( dirname "$( readlink -f "$0")" )"
+cd "$( dirname "$( readlink -f "$0")" )/.."
 
 function find_cfg_sync() {
-  PATH="../target/release:../target/debug:${PATH}" which cfgsync
+  PATH="/target/release:/target/debug:${PATH}" which cfgsync
 }
 
 CFGSYNC="$(realpath "${CFGSYNC:-$(find_cfg_sync)}")"
-E2E_TEST_DIR="$(realpath ./_tmp )/"
+E2E_TEST_DIR="$(realpath ./e2e-tests/_tmp )/"
 export CFGSYNC E2E_TEST_DIR
+
+cd e2e-tests
 
 deno test --allow-write --allow-sys --allow-read --allow-env --allow-run "$@"
