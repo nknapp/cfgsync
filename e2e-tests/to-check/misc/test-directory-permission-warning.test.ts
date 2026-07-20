a1dc82d704f3ade2dba4b6205a07bf299f17366e
Nils Knappmeier <npm@knappi.org>
2026-07-20 13:20:08 +0200

    test: replace CONFIG_TOML and STATE_FILE with variables

--- e2e-tests/to-check/misc/test-directory-permission-warning.test.ts
+++ e2e-tests/to-check/misc/test-directory-permission-warning.test.ts
@@ -1,4 +1,4 @@
-import { deindent, TestBed } from "@/lib/index.ts";
+import { CONFIG_TOML, deindent, TestBed } from "@/lib/index.ts";
 
 Deno.test("directory-permission-warning", async (t) => {
   const { testbed } = await TestBed.create(t, {
@@ -10,7 +10,7 @@ Deno.test("directory-permission-warning", async (t) => {
       globs = ["**/*"]
     `,
     files: [
-      "user:user | 755 | 0 | config.toml | __CONFIG_TOML__",
+      `user:user | 644 | 0 | config.toml | ${CONFIG_TOML}`,
       "user:user | 755 | 0 | source/",
       "user:user | 755 | 0 | source/subdir/",
       "user:user | 644 | 0 | source/subdir/file.txt | hello",
