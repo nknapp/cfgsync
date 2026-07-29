3469e92f7defd2a609868e0c30e4026851f3415b
Nils Knappmeier <npm@knappi.org>
2026-07-29 18:41:05 +0200

    chore: test init speedup

--- e2e-tests/run.sh
+++ e2e-tests/run.sh
@@ -9,5 +9,5 @@ if [[ "${CFGSYNC:+x}" ]] ; then
   export CFGSYNC="${PROJECT_DIR}/${CFGSYNC}"
 fi
 
-deno test --config deno.json --frozen --allow-write --allow-sys --allow-read --allow-env --allow-run --allow-net "$@"
+deno test --cached-only --config deno.json --frozen --allow-write --allow-sys --allow-read --allow-env --allow-run --allow-net "$@"
 
